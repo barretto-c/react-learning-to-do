de6c11c488d388ae65a17064fa21a841615eb4a7
Clyde Barretto <barretto.clyde@gmail.com>
2020-06-27 12:48:32 -0400

    Added stable look etc.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
+import { useState } from "react";
+
 import logo from './logo.svg';
 import './App.css';
 
+// https://reactjs.org/docs/hooks-intro.html
+
+const Todo = ({ todo }) => <div className="todo">{todo.text}</div>;
+
 function App() {
+
+  const [todos, setTodos] = useState([
+    { text: "Review AWS Honeycode" },
+    { text: "Review AWS Amplify" },
+    { text: "Review AWS Solutions Constructs" },
+  ]);
+
+  // we'll render our todos here ...
+  // return <div></div>
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn
         </a>
+
+        {/* https://reactjs.org/docs/lists-and-keys.htm */}
+        
       </header>
+
+      <div className="todo-list">
+          {todos.map((todo, index) => (
+            <Todo
+              key={index}
+              index={index}
+              todo={todo}
+            />
+          ))}
+      </div>
+
     </div>
   );
 }
+
 
 export default App;
