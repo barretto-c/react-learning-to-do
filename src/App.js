c10abc8a76b6489d7fd33a5fdae51c94641e79f3
Clyde Barretto <barretto.clyde@gmail.com>
2020-06-27 19:13:40 -0400

    Committed version

--- src/App.js
+++ src/App.js
@@ -6,18 +6,76 @@ import './App.css';
 
 // https://reactjs.org/docs/hooks-intro.html
 
-const Todo = ({ todo }) => <div className="todo">{todo.text}</div>;
+
+function Todo({ todo, index, completeTodo, removeTodo }) {
+  return (
+    <div
+      className="todo"
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+    >
+      {todo.text}
+
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+        <button onClick={() => removeTodo(index)}>x</button>
+      </div>
+    </div>
+  );
+}
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    // If empty return don'add
+    if (!value) 
+      return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+
+    </form>
+  );
+}
 
 function App() {
 
   const [todos, setTodos] = useState([
-    { text: "Review AWS Honeycode" },
-    { text: "Review AWS Amplify" },
-    { text: "Review AWS Solutions Constructs" },
+    { text: "Review AWS Honeycode", isCompleted: false },
+    { text: "Review AWS Amplify" , isCompleted: false },
+    { text: "Review AWS Solutions Constructs" , isCompleted: false },
   ]);
 
+  
   // we'll render our todos here ...
   // return <div></div>
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
 
   return (
     <div className="App">
@@ -42,13 +100,16 @@ function App() {
               key={index}
               index={index}
               todo={todo}
+              completeTodo={completeTodo}
+              removeTodo={removeTodo}
             />
           ))}
       </div>
+      <TodoForm addTodo={addTodo} />
+
 
     </div>
   );
 }
-
 
 export default App;
