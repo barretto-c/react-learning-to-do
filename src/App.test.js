566bb39306c70d348c36f4c22fd140d7fb8a0a4a
Clyde Barretto <barretto.clyde@gmail.com>
2020-06-30 00:12:49 -0400

    Added yarn.lock and added test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+test('Track your TODOs', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Track your TODOs/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Enter New TODO/i);
   expect(linkElement).toBeInTheDocument();
 });
